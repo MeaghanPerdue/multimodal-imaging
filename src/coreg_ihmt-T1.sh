c59b2a9adbec6ead0d349630a4597981622c5202
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-14 15:52:51 -0700

    uncomment bet

--- src/coreg_ihmt-T1.sh
+++ src/coreg_ihmt-T1.sh
@@ -3,7 +3,7 @@
 #Meaghan Perdue 14 Nov 2022
 
 for i in $(cat subjlist.txt); do
-	#bet /Volumes/catherine_team/mvperdue/preschool/mrs/data_lag/*${i}*/s${i}*.nii /Volumes/catherine_team/mvperdue/preschool/multimodal/data/${i}/${i}_T1_bet.nii.gz
+	bet /Volumes/catherine_team/mvperdue/preschool/mrs/data_lag/*${i}*/s${i}*.nii /Volumes/catherine_team/mvperdue/preschool/multimodal/data/${i}/${i}_T1_bet.nii.gz
 	cd /Volumes/catherine_team/mvperdue/preschool/multimodal/data/${i}
 	flirt -in /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data/${i}/${i}_qihMTMaskedThresh.nii.gz -ref ${i}_T1_bet.nii.gz -out ${i}_qihMT_reg2T1.nii.gz
 	cd /Volumes/catherine_team/mvperdue/preschool/multimodal/data
