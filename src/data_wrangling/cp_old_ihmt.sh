f6a11b86f1cda94575f1facb71e4b790f79f55ea
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-17 10:58:20 -0700

    set directory path variables

--- src/data_wrangling/cp_old_ihmt.sh
+++ src/data_wrangling/cp_old_ihmt.sh
@@ -4,14 +4,15 @@
 #script to copy old ihmt data (from Jess R. processing) to working directory
 #copying masked, thresholded qihMT maps
 
-export WD=/Volumes/catherine_team/mvperdue/preschool/multimodal/data
+export WD="/Volumes/catherine_team/mvperdue/preschool/multimodal/data"
+export IHMT_DIR="/Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/qihMT original data"
 
-
-for i in $(cat subjlist.txt); do
+for i in $(cat /Volumes/catherine_team/mvperdue/preschool/multimodal/src/subjlist_old.txt); do
 	mkdir $WD/${i}
-	cp -Rv /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/qihMT original data/${i}/autoReg/qihMT/qihMTBrain.nii $WD/${i}
+	cp -Rv $IHMT_DIR/${i}/autoReg/qihMT/qihMTBrain.nii $WD/${i}
 	cd $WD/${i} 
-	mv qihMTBrain.nii ${i}_qihMTMaskedThresh.nii.gz
+	gzip qihMTBrain.nii
+	mv qihMTBrain.nii.gz ${i}_qihMTMaskedThresh.nii.gz
 	cd $WD
 done
 
