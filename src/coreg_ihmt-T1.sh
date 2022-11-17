05db0a971d4bb06a78d2e89ed7b4ae0bf7347f2d
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-17 15:17:45 -0700

    edit file names for 2019-2020 data

--- src/coreg_ihmt-T1.sh
+++ src/coreg_ihmt-T1.sh
@@ -5,7 +5,19 @@
 export WD=/Volumes/catherine_team/mvperdue/preschool/multimodal/data
 
 for i in $(cat subjlist.txt); do
+	mkdir $WD/${i}
 	bet /Volumes/catherine_team/mvperdue/preschool/mrs/data_lag/*${i}*/s${i}*.nii $WD/${i}/${i}_T1_bet.nii.gz
+	cd $WD/${i}
+	cp -v /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data/${i}/${i}_qihMTMaskedThresh.nii.gz .
+	flirt -in ${i}_qihMTMaskedThresh.nii.gz -ref ${i}_T1_bet.nii.gz -out ${i}_qihMT_reg2T1.nii.gz
+	cd $WD
+done
+
+
+
+#subjects from 2019-2020 failed due to naming of T1w .nii files, insert "PRESCHOOL" into filename and rerun
+for i in $(cat subjlist_PS19-20.txt); do
+	bet /Volumes/catherine_team/mvperdue/preschool/mrs/data_lag/*${i}*/sPRESCHOOL${i}*.nii $WD/${i}/${i}_T1_bet.nii.gz
 	cd $WD/${i}
 	cp -v /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data/${i}/${i}_qihMTMaskedThresh.nii.gz .
 	flirt -in ${i}_qihMTMaskedThresh.nii.gz -ref ${i}_T1_bet.nii.gz -out ${i}_qihMT_reg2T1.nii.gz
