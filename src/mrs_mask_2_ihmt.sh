7b28bcebe557961729ffd2a2a71576e202caa0d3
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-21 16:28:55 -0700

    fixed wm mask .nii file names

--- src/mrs_mask_2_ihmt.sh
+++ src/mrs_mask_2_ihmt.sh
@@ -6,7 +6,7 @@
 
 for i in $(cat subjlist.txt); do
 	cd /Volumes/catherine_team/mvperdue/preschool/multimodal/data/${i}
-	fslmaths /Volumes/catherine_team/mvperdue/preschool/mrs/data_lag/*${i}*/PS10085_PS19_001_11288_P67072_mask_WM.nii -bin ${i}_mrs_mask_wm.nii
+	fslmaths /Volumes/catherine_team/mvperdue/preschool/mrs/data_lag/*${i}*/*mask_WM.nii -bin ${i}_mrs_mask_wm.nii
 	fslmaths ${i}_qihMT_reg2T1.nii.gz -mul ${i}_mrs_mask_wm.nii ${i}_qihMT_mrs_mask_wm.nii.gz
 	fslstats ${i}_qihMT_mrs_mask_wm.nii.gz -M -S >> ${i}_qihMT_LTP-WM_M_SD.txt
 	cd /Volumes/catherine_team/mvperdue/preschool/multimodal/data
