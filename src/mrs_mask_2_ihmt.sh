cda7c5db6bc3634bc9f71133a4f115f7db67eb16
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-22 16:39:18 -0700

    cleaned up script, write full output file

--- src/mrs_mask_2_ihmt.sh
+++ src/mrs_mask_2_ihmt.sh
@@ -13,33 +13,43 @@ export WD=/Volumes/catherine_team/mvperdue/preschool/multimodal/data
 #set MRS data directory
 export MRS_DIR=/Volumes/catherine_team/mvperdue/preschool/mrs/data_lag
 
-for i in $(cat ${SRC}/tmp_subs.txt); do
-	cd ${WD}/${i}
-	fslmaths ${MRS_DIR}/*${i}*/*mask_WM.nii -bin ${i}_mrs_mask_wm.nii
-	fslmaths ${i}_qihMT_reg2T1.nii.gz -mul ${i}_mrs_mask_wm.nii ${i}_qihMT_mrs_mask_wm.nii.gz
-	fslstats ${i}_qihMT_mrs_mask_wm.nii.gz -M -S >> ${i}_qihMT_LTP-WM_M_SD.txt
-	cd ${WD}
-done
+# create mask of whole MRS voxel and apply to qihMT map
 
-
-# apply whole MRS voxel mask to qihMT map and calculate mean qihMT across voxels, write to output txt file
-
-for i in $(cat ${SRC}/tmp_subs.txt); do
+for i in $(cat ${SRC}/subjlist.txt); do
 	cd ${WD}/${i}
 	fslmaths ${MRS_DIR}/*${i}*/*mask.nii -bin ${i}_mrs_mask.nii
 	fslmaths ${i}_qihMT_reg2T1.nii.gz -mul ${i}_mrs_mask.nii ${i}_qihMT_mrs_mask.nii.gz
-	echo $i $(fslstats ${i}_qihMT_mrs_mask.nii.gz -M -S) >> ../all_qihMT_LTP-M_SD.txt
 	cd ${WD}
 done
 
 
-# create tissue mask of excluding CSF, apply to qihMT map, and calculate mean qihMT across voxels, write to output txt file (for exclusion of CSF)
+# create tissue mask of excluding CSF, apply to qihMT map, and calculate mean qihMT across voxels
 
-for i in $(cat ${SRC}/tmp_subs.txt); do
+for i in $(cat ${SRC}/subjlist.txt); do
 	cd ${WD}/${i}
 	fslmaths ${MRS_DIR}/*${i}*/*mask_CSF.nii -bin ${i}_mrs_mask_csf.nii 
 	fslmaths ${i}_mrs_mask.nii -sub ${i}_mrs_mask_csf.nii ${i}_mrs_mask_gm+wm.nii
 	fslmaths ${i}_qihMT_reg2T1.nii.gz -mul ${i}_mrs_mask_gm+wm.nii ${i}_qihMT_mrs_mask_gm+wm.nii.gz
-	echo $i $(fslstats ${i}_qihMT_mrs_mask_gm+wm.nii.gz -M -S) >> ../all_qihMT_LTP-M_SD_GM+WM.txt
 	cd ${WD}
 done
+
+#create mask of white matter within MRS voxel and apply to qihMT map
+# for i in $(cat ${SRC}/subjlist.txt); do
+# 	cd ${WD}/${i}
+# 	fslmaths ${MRS_DIR}/*${i}*/*mask_WM.nii -bin ${i}_mrs_mask_wm.nii
+# 	fslmaths ${i}_qihMT_reg2T1.nii.gz -mul ${i}_mrs_mask_wm.nii ${i}_qihMT_mrs_mask_wm.nii.gz
+# 	cd ${WD}
+# done
+
+# write qihmt mean and sd values into 1 txt file
+# first set up txt file header row
+# cd ${WD}
+# echo study_code		qihmt_ltp_m 	qihmt_ltp_sd 	qihmt_wm+gm_mean 	qihmt_wm+gm_sd	qihmt_wm_m	qihmt_wm_sd	 >> qihMT_LTP_M-SD_all.txt
+	
+# write qihMT mean and sd values from each mask into output file
+for i in $(cat ${SRC}/subjlist.txt); do
+	cd ${WD}/${i}
+	echo ${i} 	$(fslstats ${i}_qihMT_mrs_mask.nii.gz -M -S) 	$(fslstats ${i}_qihMT_mrs_mask_gm+wm.nii.gz -M -S) 	$(fslstats ${i}_qihMT_mrs_mask_wm.nii.gz -M -S) >> ${WD}/qihMT_LTP_M-SD_all.txt
+	cd ${WD}
+done
+
